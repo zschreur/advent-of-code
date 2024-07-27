1156c29793200b5cd506f76e4a04c02651c1424f
Zackary Schreur <z.schreur@techsmith.com>
2024-07-27 09:51:48 -0400

    Add nextLine function

--- src/file.zig
+++ src/file.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
+pub fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
+    const line = (try reader.readUntilDelimiterOrEof(
+        buffer,
+        '\n',
+    )) orelse return null;
+
+    // trim annoying windows-only carriage return character
+    if (@import("builtin").os.tag == .windows) {
+        return std.mem.trimRight(u8, line, "\r");
+    } else {
+        return line;
+    }
+}
+
 pub fn openInputFile(day: usize) !std.fs.File {
     const input_dir = try std.fs.cwd().openDir("input", .{ .access_sub_paths = false });
     var buffer: [100]u8 = undefined;
