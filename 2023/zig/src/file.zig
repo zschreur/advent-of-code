faf793e42dd559cbd448b91334b630557e37f918
Zackary Schreur <z.schreur@techsmith.com>
2024-08-06 16:47:42 -0400

    chore: move zig 2023 files into 2023/zig

--- 2023/zig/src/file.zig
+++ 2023/zig/src/file.zig
@@ -15,7 +15,7 @@ pub fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
 }
 
 pub fn openInputFile(day: usize) !std.fs.File {
-    const input_dir = try std.fs.cwd().openDir("input", .{ .access_sub_paths = false });
+    const input_dir = try std.fs.cwd().openDir("../../puzzle-inputs/", .{ .access_sub_paths = false });
     var buffer: [100]u8 = undefined;
     const file_name = try std.fmt.bufPrint(&buffer, "day-{s}.txt", .{std.fmt.digits2(day)});
     return try input_dir.openFile(file_name, .{ .mode = .read_only });
